ec45769b5533f9959cab573b2464c422a89ec627
jbroseman <jbroseman@gmail.com>
2015-05-14 17:01:44 -0700

    added set-payouts html and js

--- www/scripts/game.js
+++ www/scripts/game.js
@@ -35,8 +35,53 @@
         $('#Modal_Overlay').addClass('hidden');
         $('.popup.new-game').addClass('hidden');
     });
+    
+    $('#Configure_Payouts').on('click', function () {
+        $('#Modal_Overlay').removeClass('hidden');
+        $('.popup.set-payouts').removeClass('hidden');
+    });
+
+    $('#Button_Upsert_Payouts').on('click', function () {
+        upsertPayouts();
+    });
+
+    $('.button.close-popup').on('click', function () {
+        $('#Modal_Overlay').addClass('hidden');
+        $('.popup').addClass('hidden');
+    });
 
     function upsertGame() {
+        var data = {
+            'Date': $('#Date').val(),
+            'BlindIncrementID': $('#BlindIncrementID').val(),
+            'BuyInID': $('#BuyinID').val(),
+            'BeginningStack': $('#BeginningStack').val()
+        };
+
+        $.ajax({
+            url: "api/upsertgame.php",
+            type: "post",
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+        }).done(function (data) {
+            if (data)
+            {
+                if (data.message) {
+                    $('#result_message').html(data.message);
+                }
+                else {
+                    $('#modal_overlay').addClass('hidden');
+                    $('.popup.new-game').addClass('hidden');
+                    location.reload();
+                }
+            }
+        })
+        .error(function (e) {
+            alert(e.responseText);
+        });
+    }
+
+    function upsertPayouts(data) {
         var data = {
             'Date': $('#Date').val(),
             'BlindIncrementID': $('#BlindIncrementID').val(),
