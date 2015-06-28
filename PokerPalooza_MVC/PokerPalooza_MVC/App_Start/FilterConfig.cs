ffe5a4166537281924a937c591dfd76e1a1269b7
jbroseman <jbroseman@gmail.com>
2015-06-28 08:42:37 -0700

    create mvc project

---
+++ PokerPalooza_MVC/PokerPalooza_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PokerPalooza_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PokerPalooza_MVC/PokerPalooza_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokerPalooza_MVC.MvcApplication" Language="C#" %>
