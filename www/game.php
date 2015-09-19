a57f38478962dc7e3013cd1962406cd8214a3427
jbroseman <kerplunk79@hotmail.com>
2015-09-19 16:43:33 -0700

    tooko out debugging html

--- www/game.php
+++ www/game.php
@@ -5,7 +5,7 @@
 			$totalpot = (($game['BuyInAmount'] * $buyincount) + ($game['BumpCost'] * $bumpcount));
 			$chipsinplay = ((($buyincount * $game['BeginningStack']) + $bonuschips) + ($bumpcount * $game['BumpStack']));
 		?>
-			<div class="total-pot"><label>Total Pot Player count <?=$game['PlayerCount']?></label><span>$<?=number_format($totalpot, 2)?></span></div>
+			<div class="total-pot"><label>Total Pot</label><span>$<?=number_format($totalpot, 2)?></span></div>
 			<div class="chips-in-play"><label>Chips in play</label><span>$<?=number_format($chipsinplay, 0)?></span></div>
 	</div>
 <?php } ?>
