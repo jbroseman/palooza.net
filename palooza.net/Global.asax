8b6e8e4ff3ae2cea7c3e8ad7aff073b9a966bd63
Stephenson, Scott <sstephenson@russell.com>
2016-03-31 21:26:56 -0700

    create solution

---
+++ palooza.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="palooza.net.MvcApplication" Language="C#" %>
---
+++ palooza.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(palooza.net.Startup))]
+namespace palooza.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
