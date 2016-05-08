d9d28b96a8c07b81786d4f29d68aab36df7cc6da
jbroseman <kerplunk79@hotmail.com>
2016-05-08 10:08:50 -0700

    Building views and models

---
+++ palooza.net/Models/ViewModels/ChipsPartialVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Palooza.Net.Models.ViewModels
+{
+    public class ChipsPartialVM
+    {
+    }
+}
---
+++ palooza.net/Models/ViewModels/GameDashboardVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Palooza.Net.Web.Models.Components;
+
+namespace Palooza.Net.Web.Models.ViewModels
+{
+    public class GameDashboardVM
+    {
+        public List<GamePlayer> Players { get; set; }
+        public List<GameBlind> Blinds { get; set; }
+        public GameDetailsPartialVM Game { get; set; }
+
+        public GameDashboardVM()
+        {
+            Players = new List<GamePlayer>();
+            Blinds = new List<GameBlind>();
+            Game = new GameDetailsPartialVM();
+        }
+    }
+}
