736060362ac04c32a4cb5a9fe946f19c8d9e4e03
scooter <yalbik@yahoo.com>
2017-01-04 20:58:19 -0800

    blinds will be ints and not collections of chips

--- palooza.domain/Models/Blind.cs
+++ palooza.domain/Models/Blind.cs
@@ -4,7 +4,12 @@ namespace palooza.domain.Models
 {
     public class Blind
     {
-        public IEnumerable<Chip> Small;
-        public IEnumerable<Chip> Big;
+        public int Big;
+        public int Small;
+
+        public bool Equals(Blind blind)
+        {
+            return blind.Big == Big && blind.Small == Small;
+        }
     }
 }
